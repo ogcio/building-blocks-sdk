20f6b809628eaa1ed3352d15c478fc19fc5c5dc7
Francesco Maida <francescogithub@efem.dev>
2024-10-04 15:11:46 +0200

    chore(core): setup devex, biome and removed monorepo (#1)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,6 @@
-module.exports = {
+export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [
-      2,
-      "always",
-      [
-        "core",
-        "building-blocks-sdk"
-      ],
-    ],
+    "scope-enum": [2, "always", ["core", "building-blocks-sdk"]],
   },
 };
