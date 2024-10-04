dcab6fab78bdd868dde6834ae00233580d339f3c
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-04 11:03:24 +0200

    feat(core): first commit

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "core",
+        "building-blocks-sdk"
+      ],
+    ],
+  },
+};
