0fdfab41e05a181d9295317c762c6a561d14c9c9
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-08 08:19:33 +0200

    feat: working on getting clients

--- src/cli/updateClientsCommand.ts
+++ src/cli/updateClientsCommand.ts
@@ -1,8 +1,10 @@
+import fs from "node:fs";
 import { resolve } from "node:path";
-import { Command } from "commander";
-
 import { type Static, Type } from "@sinclair/typebox";
 import { Value } from "@sinclair/typebox/value";
+import { Command } from "commander";
+import { parse } from "yaml";
+
 enum OpenAPIFileFormats {
   JSON = "json",
   YAML = "yaml",
@@ -19,14 +21,17 @@ export type ClientsConfiguration = Static<typeof ClientsConfigurationSchema>;
 export const ConfigurationFileSchema = Type.Object({
   buildingBlocks: Type.Array(ClientsConfigurationSchema),
 });
-// Define a schema for a User object
+export type ConfigurationFile = Static<typeof ConfigurationFileSchema>;
 
-const getAbsolutePathFromOption = (relativeInputPath: string): string =>
-  resolve(process.cwd(), relativeInputPath);
+const CLIENTS_ROOT_FOLDER_PATH = "src/clients";
 
-async function updateClients({
-  configurationFilePath,
-}: { configurationFilePath: string }): Promise<void> {
+function getAbsolutePathFromOption(...relativeInputPath: string[]): string {
+  return resolve(process.cwd(), ...relativeInputPath);
+}
+
+async function readConfigurationFile(
+  configurationFilePath: string,
+): Promise<ConfigurationFile> {
   const { default: rawConfigurationFile } = await import(
     configurationFilePath,
     {
@@ -36,10 +41,65 @@ async function updateClients({
     }
   );
 
-  const parsedConfiguration = Value.Parse(
-    ConfigurationFileSchema,
-    rawConfigurationFile,
+  return Value.Parse(ConfigurationFileSchema, rawConfigurationFile);
+}
+
+async function getOpenApiDefinitionFileContent(
+  filePath: string,
+): Promise<string> {
+  if (fs.existsSync(filePath)) {
+    return fs.readFileSync(filePath, "utf-8");
+  }
+
+  try {
+    const url = new URL(filePath);
+    const response = await fetch(url);
+    return response.text();
+  } catch {}
+
+  throw new Error(
+    "The configuration-file-path is not a valid file path nor url",
   );
+}
+
+async function processBuildingBlock({
+  inputBuildingBlock,
+}: { inputBuildingBlock: ClientsConfiguration }): Promise<void> {
+  const openApiContent = await getOpenApiDefinitionFileContent(
+    inputBuildingBlock.openApiDefinitionUrl,
+  );
+  const openApiParsed =
+    inputBuildingBlock.openApiDefinitionFormat === OpenAPIFileFormats.JSON
+      ? JSON.parse(openApiContent)
+      : parse(openApiContent);
+
+  const folderFullPath = getAbsolutePathFromOption(
+    CLIENTS_ROOT_FOLDER_PATH,
+    inputBuildingBlock.name,
+  );
+
+  if (!fs.existsSync(folderFullPath)) {
+    fs.mkdirSync(folderFullPath, { recursive: true });
+  }
+}
+
+async function processBuildingBlocks({
+  inputBuildingBlocks,
+}: { inputBuildingBlocks: ClientsConfiguration[] }): Promise<void> {
+  const promises: Promise<void>[] = [];
+  for (const inputBuilding of inputBuildingBlocks) {
+    promises.push(processBuildingBlock({ inputBuildingBlock: inputBuilding }));
+  }
+}
+
+async function updateClients({
+  configurationFilePath,
+}: { configurationFilePath: string }): Promise<void> {
+  const configurationFile = await readConfigurationFile(configurationFilePath);
+
+  processBuildingBlocks({
+    inputBuildingBlocks: configurationFile.buildingBlocks,
+  });
 }
 
 const updateClientsCommand = new Command("clients:update");
