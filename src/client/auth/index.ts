ecf9fffc5d73a17d8ce612a1f115af062b853f92
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-10 11:09:23 +0200

    chore: fixed logto management

---
+++ src/client/auth/index.ts
@@ -0,0 +1,75 @@
+import type {
+  GetAccessTokenParams,
+  GetOrganizationTokenParams,
+  TokenResponseBody,
+} from "../../types/index.js";
+
+export const fetchToken = async (params: {
+  logtoOidcEndpoint: string;
+  applicationId: string;
+  applicationSecret: string;
+  scopes: string[];
+  specificBodyFields: { [x: string]: string };
+}) => {
+  const body = {
+    ...params.specificBodyFields,
+    scope: params.scopes.join(" "),
+    grant_type: "client_credentials",
+  };
+  const logtoOidcEndpoint = params.logtoOidcEndpoint.endsWith("/")
+    ? params.logtoOidcEndpoint
+    : `${params.logtoOidcEndpoint}/`;
+  const response = await fetch(`${logtoOidcEndpoint}token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(
+        `${params.applicationId}:${params.applicationSecret}`,
+      ).toString("base64")}`,
+    },
+    body: new URLSearchParams(body).toString(),
+  });
+
+  if (response.status !== 200) {
+    throw new Error("Unauthorized");
+  }
+
+  return response.json() as Promise<TokenResponseBody>;
+};
+
+export const getAccessToken = async (params: GetAccessTokenParams) => {
+  const tokenResponse = await fetchToken({
+    ...params,
+    specificBodyFields: { resource: params.resource },
+  });
+  return tokenResponse.access_token;
+};
+
+export const getOrganizationToken = async (
+  params: GetOrganizationTokenParams,
+) => {
+  const { UserScope } = await importUserScopes();
+
+  const tokenResponse = await fetchToken({
+    ...params,
+    scopes: [
+      ...params.scopes,
+      UserScope.OrganizationRoles,
+      UserScope.Organizations,
+    ],
+    specificBodyFields: {
+      organization_id: params.organizationId,
+    },
+  });
+  return tokenResponse.access_token;
+};
+
+const importUserScopes = async () => {
+  try {
+    const { UserScope } = await import("@logto/node");
+
+    return { UserScope };
+  } catch {
+    throw new Error("@logto/node package is not installed!");
+  }
+};
