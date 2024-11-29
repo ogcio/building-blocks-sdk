a18a4b455fb03b5efc3ab40378b03f85d20049b3
Sara Zanellato <sara.zanellato@nearform.com>
2024-11-29 15:15:53 +0100

    chore: update payments schema

--- src/client/clients/payments/schema.ts
+++ src/client/clients/payments/schema.ts
@@ -877,83 +877,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/api/v1/requests/allActive": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description Default Response */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": {
-                            data: {
-                                paymentRequestId: string;
-                                title: string;
-                                description?: string;
-                                amount?: number;
-                                allowAmountOverride: boolean;
-                                createdAt: string;
-                            }[];
-                            metadata?: {
-                                /** @description Object containing the links to the related endpoints */
-                                links?: {
-                                    self: {
-                                        /** @description URL pointing to the request itself */
-                                        href?: string;
-                                    };
-                                    next?: {
-                                        /** @description URL pointing to the next page of results in a paginated response. If there are no more results, this field may be omitted */
-                                        href?: string;
-                                    };
-                                    prev?: {
-                                        /** @description URL pointing to the previous page of results in a paginated response. If there are no more results, this field may be omitted */
-                                        href?: string;
-                                    };
-                                    first: {
-                                        /** @description URL pointing to the first page of results in a paginated response */
-                                        href?: string;
-                                    };
-                                    last: {
-                                        /** @description URL pointing to the first page of results in a paginated response */
-                                        href?: string;
-                                    };
-                                    /** @description It may contain a list of other useful URLs, e.g. one entry for page:'page 1', 'page 2' */
-                                    pages: {
-                                        [key: string]: {
-                                            href?: string;
-                                        };
-                                    };
-                                };
-                                /** @description Number representing the total number of available items */
-                                totalCount?: number;
-                            };
-                        };
-                    };
-                };
-            };
-        };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
     "/api/v1/requests/{requestId}": {
         parameters: {
             query?: never;
@@ -2607,14 +2530,49 @@ export interface paths {
                     };
                     content: {
                         "application/json": {
-                            ACCOUNT: string;
-                            AMOUNT: string;
-                            CURRENCY: string;
-                            MERCHANT_ID: string;
-                            ORDER_ID: string;
-                            TIMESTAMP: string;
-                            URL: string;
-                            SHA256HASH: string;
+                            data: {
+                                ACCOUNT: string;
+                                AMOUNT: string;
+                                CURRENCY: string;
+                                MERCHANT_ID: string;
+                                ORDER_ID: string;
+                                TIMESTAMP: string;
+                                URL: string;
+                                SHA256HASH: string;
+                            };
+                            metadata?: {
+                                /** @description Object containing the links to the related endpoints */
+                                links?: {
+                                    self: {
+                                        /** @description URL pointing to the request itself */
+                                        href?: string;
+                                    };
+                                    next?: {
+                                        /** @description URL pointing to the next page of results in a paginated response. If there are no more results, this field may be omitted */
+                                        href?: string;
+                                    };
+                                    prev?: {
+                                        /** @description URL pointing to the previous page of results in a paginated response. If there are no more results, this field may be omitted */
+                                        href?: string;
+                                    };
+                                    first: {
+                                        /** @description URL pointing to the first page of results in a paginated response */
+                                        href?: string;
+                                    };
+                                    last: {
+                                        /** @description URL pointing to the first page of results in a paginated response */
+                                        href?: string;
+                                    };
+                                    /** @description It may contain a list of other useful URLs, e.g. one entry for page:'page 1', 'page 2' */
+                                    pages: {
+                                        [key: string]: {
+                                            href?: string;
+                                        };
+                                    };
+                                };
+                                /** @description Number representing the total number of available items */
+                                totalCount?: number;
+                            };
                         };
                     };
                 };
