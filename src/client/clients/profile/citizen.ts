9e325caedd22c6b2c4bd204bed1ebd6357b9cfb0
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-07-30 11:58:27 +0200

    feat: started adding consents features

---
+++ src/client/clients/profile/citizen.ts
@@ -0,0 +1,34 @@
+import type createClient from "openapi-fetch";
+import type { paths } from "./schema.js";
+
+export class ProfileCitizen {
+  constructor(
+    private readonly client: ReturnType<typeof createClient<paths>>,
+  ) {}
+
+  async getLatestConsent(
+    query: paths["/api/v1/citizens/consents/latest"]["get"]["parameters"]["query"],
+  ) {
+    return this.client.GET("/api/v1/citizens/consents/latest", {
+      params: { query },
+    });
+  }
+
+  async submitConsent(
+    body: NonNullable<
+      paths["/api/v1/citizens/consents/"]["post"]["requestBody"]
+    >["content"]["application/json"],
+  ) {
+    return this.client.POST("/api/v1/citizens/consents/", {
+      body,
+    });
+  }
+
+  async listConsents(
+    query: paths["/api/v1/citizens/consents/"]["get"]["parameters"]["query"],
+  ) {
+    return this.client.GET("/api/v1/citizens/consents/", {
+      params: { query },
+    });
+  }
+}
